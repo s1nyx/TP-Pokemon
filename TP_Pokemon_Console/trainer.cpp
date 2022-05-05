a5d8adada1b434ca7b366a5894cfcfbd7edb7a44
s1nyx <sinyx.contact@gmail.com>
2022-05-05 19:25:52 +0200

    feat(Trainer): implemented addPokemon, removePokemon, compareWithOpponent methods

--- TP_Pokemon_Console/trainer.cpp
+++ TP_Pokemon_Console/trainer.cpp
@@ -1,6 +1,6 @@
 #include "trainer.h"
 #include <iostream>
-#include <numeric>
+#include <algorithm>
 
 int Trainer::getItsLevel() const
 {
@@ -56,6 +56,59 @@ Trainer::~Trainer()
     delete itsPokemons;
 }
 
+void Trainer::addPokemon(Pokemon *pokemon)
+{
+    itsPokemons->push_back(pokemon);
+
+    addXP(1);
+
+    std::cout << "Pokemon " << pokemon->getItsName() << " ajouté à la collection" << std::endl;
+}
+
+void Trainer::removePokemon(Pokemon *pokemon)
+{
+    if (itsXP < 2)
+    {
+        std::cout << "Oups! Vous devez avoir au minimum 2 XP pour retirer ce pokemon" << std::endl;
+    }
+    else
+    {
+        std::vector<Pokemon*>::iterator pokemonIterator = std::find(itsPokemons->begin(), itsPokemons->end(), pokemon);
+
+        if (pokemonIterator != itsPokemons->end())
+        {
+            itsPokemons->erase(pokemonIterator);
+        }
+    }
+}
+
+void Trainer::compareWithOpponent(Trainer *opponent)
+{
+    std::cout << *this << std::endl;
+    std::cout << *opponent << std::endl;
+
+    unsigned int currentTrainerWinningProbability = getTotalStrengthPower() / getTotalHealthPoints() * 100;
+    unsigned int opponentTrainerWinningProbability = getTotalStrengthPower() / getTotalHealthPoints() * 100;
+
+    if (currentTrainerWinningProbability > opponentTrainerWinningProbability)
+    {
+        std::cout << itsName << " a plus de chance de gagné que " << opponent->itsName << "(" << currentTrainerWinningProbability << "% vs " << opponentTrainerWinningProbability << "%)";
+    }
+    else if (currentTrainerWinningProbability < opponentTrainerWinningProbability)
+    {
+        std::cout << opponent->itsName << " a plus de chance de gagné que " << itsName << "(" << opponentTrainerWinningProbability << "% vs " << currentTrainerWinningProbability << "%)";
+    }
+    else
+    {
+        std::cout << "Les deux dresseurs ont autant de chance de gagné." << std::endl;
+    }
+}
+
+/*!
+ * Permet d'ajouter de l'XP (points) et de faire levelup le dresseur
+ * \brief Trainer::addXP
+ * \param xp
+ */
 void Trainer::addXP(int xp)
 {
     if (itsXP + xp >= 10)
